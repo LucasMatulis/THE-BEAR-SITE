db1ec52b3c43e91f419ff034f8ca4e33817531c3
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2024-05-18 14:48:52 -0300

    Adicionando Data as denuncias e criando os models

--- BACKEND/src/main/java/br/com/curso/faculdade/entities/Denuncia.java
+++ BACKEND/src/main/java/br/com/curso/faculdade/entities/Denuncia.java
@@ -1,37 +1,51 @@
 package br.com.curso.faculdade.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity(name = "Denuncia")
 public class Denuncia implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "ID")
+    @Column(name = "ID")
     int id;
 
-    @Column (name = "Nome")
+    @Column(name = "Nome")
     String nome;
 
-    @Column (name ="Texto")
+    @Column(name = "Texto")
     String texto;
 
-    public Denuncia(){}
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "dataCriacao")
+    private Date dataCriacao;
 
-    public Denuncia(int id,String nome, String texto){
-        this.id=id;
-        this.nome=nome;
-        this.texto=texto;
+    public Denuncia() {}
+
+    public Denuncia(int id, String nome, String texto) {
+        this.id = id;
+        this.nome = nome;
+        this.texto = texto;
     }
 
-    public Denuncia(String texto){
-        this.texto=texto;
+    public Denuncia(String texto) {
+        this.texto = texto;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        dataCriacao = new Date();
     }
 
     public int getId() {
@@ -57,6 +71,12 @@ public class Denuncia implements Serializable {
     public void setTexto(String texto) {
         this.texto = texto;
     }
-    
-    
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 }
