9dddde34d346b617c1eb9d55ef9cc227454bf946
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2024-05-20 10:46:42 -0300

    services completos e rotas estabelecidas

--- FRONTEND/the-bear/src/app/services/denuncia/denuncia.service.ts
+++ FRONTEND/the-bear/src/app/services/denuncia/denuncia.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Denuncia } from '../../model/denuncia';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { Observable, of } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DenunciaService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+
+  inserirDenuncia(denuncia?: Denuncia):Observable <boolean>{
+
+    if(!denuncia) return of(false);
+    return this.http.post<boolean>(`${environment.urlApi}/denuncia`, denuncia)
+  }
+
+
+  buscarDenuncia(): Observable<Denuncia[]> {
+    return this.http.get<Denuncia[]>(`${environment.urlApi}/denuncia`);
+  }
+
+  buscarDenunciaNome(nome? : string): Observable <number | undefined>{
+    if(!nome) return of (undefined);
+    return this.http.get< number > (`${environment.urlApi}/denuncia/nome/${nome}`);
+  }
+
+  removerDenuncia(id?: number) : Observable<boolean | undefined>{
+    if(!id) return of(undefined);
+    return this.http.delete<boolean>(`${environment.urlApi}/denuncia/${id}`)
+  }
+
 }
--- FRONTEND/the-bear/src/app/services/feedback/feedback.service.ts
+++ FRONTEND/the-bear/src/app/services/feedback/feedback.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Feedback } from '../../model/feedbak';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+
+  inserirFeedBack(feedbak?: Feedback):Observable <boolean>{
+
+    if(!feedbak) return of(false);
+    return this.http.post<boolean>(`${environment.urlApi}/feedback`, feedbak)
+  }
+
+
+  buscarFeedBack(): Observable<Feedback[]> {
+    return this.http.get<Feedback[]>(`${environment.urlApi}/feedback`);
+  }
+
+  buscarFeedBackNome(nome? : string): Observable <number | undefined>{
+    if(!nome) return of (undefined);
+    return this.http.get< number > (`${environment.urlApi}/feedbak/nome/${nome}`);
+  }
+
+  removerFeedBack(id?: number) : Observable<boolean | undefined>{
+    if(!id) return of(undefined);
+    return this.http.delete<boolean>(`${environment.urlApi}/feedbak/${id}`)
+  }
+
 }
