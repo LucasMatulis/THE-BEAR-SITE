cf788a8a6500d8022784f740538511e5e7a9dfd6
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2024-05-23 15:38:14 -0300

    preparando o cadastro component

--- FRONTEND/the-bear/src/app/components/cadastro/cadastro.component.ts
+++ FRONTEND/the-bear/src/app/components/cadastro/cadastro.component.ts
@@ -1,19 +1,75 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { of } from 'rxjs';
-
+import { ProdutoService } from 'src/app/services/produto/produto.service';
+import { Produto } from 'src/app/model/produto';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
 
   constructor(
-    
+    private produtoService: ProdutoService,
+    private snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
+  produtos?: Produto[];
+  produto: Produto = {
+    id: 0,
+    nome: "",
+    preco: 0,
+    descricao: "",
+    tipo: 0
+  };
+
+  ngOnInit(): void {
+    this.buscarProdutos();
+  }
+
+  criarProduto() {
+    this.produto.nome = this.produto.nome?.trim();
+    this.produto.descricao = this.produto.descricao?.trim();
+
+    const usuarioExistente = this.produtos?.find(
+      (element) => element.nome === this.produto.nome
+    );
+
+    if (usuarioExistente) {
+      this.snackBar.open("Este produto já existe", "OK!");
+    } else {
+      if (this.produto.nome !== "" && this.produto.preco > 0) {
+        this.produtoService.inserirProduto(this.produto).subscribe(
+          (response) => {
+            this.buscarProdutos();
+            this.snackBar.open("Produto cadastrado!", "OK!");
+          },
+          (error) => {
+            console.log(error);
+            this.snackBar.open("Erro ao cadastrar produto!", "OK!");
+          }
+        );
+      } else {
+        this.snackBar.open("Nome ou preço do produto inválido!", "OK!");
+      }
+    }
+  }
+
+  buscarProdutos(): void {
+    this.produtoService.buscarProduto()
+      .pipe(
+        catchError((error) => {
+          console.log(error);
+          return of([]);
+        })
+      )
+      .subscribe((produtos) => {
+        this.produtos = produtos as Produto[];
+      });
+  }
 }
